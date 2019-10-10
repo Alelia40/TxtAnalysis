6d53ccc99b50a54f3af6f1db9d96403657ab2212
Alex Li <alelia40@gmail.com>
2019-10-09 23:57:30 -0400

    adds a document upload tab and reorganizes page a bit

--- indexScripts.js
+++ indexScripts.js
@@ -2,3 +2,14 @@ function displayText() {
    var textValue = document.getElementById("textInsert").value;
 	document.getElementById("results").innerHTML = textValue;
 }
+
+function swapMethods(buttonName){
+   if(buttonName == 'file'){
+      document.getElementById('manualInput').style.display = "none";
+      document.getElementById('docUpload').style.display = "block";
+   }
+   else{
+      document.getElementById('manualInput').style.display = "block";
+      document.getElementById('docUpload').style.display = "none";
+   }
+}

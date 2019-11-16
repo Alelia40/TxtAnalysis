bea562c94699e21cf71ec079188a0795561368f7
Sean Mann <sean@seans-imac.home>
2019-11-16 16:36:56 -0500

    Added analysis toggle functionality and made some style updates

--- indexScripts.js
+++ indexScripts.js
@@ -1,35 +1,85 @@
 function onSubmitPressed() {
-   var textValue = document.getElementById("textInsert").value;
-   document.getElementById("results").innerHTML = textValue;
-   displayResultsBox();
+
+
+	//Show Input Text Toggle
+	var inputtedTextToggle = document.getElementById("inputtedTextToggle");
+	var inputtedTextDisplay = document.getElementById("inputtedTextDisplay");
+	if (inputtedTextToggle.checked == true){
+		inputtedTextDisplay.style.display = "block";
+		var textValue = document.getElementById("textInputContents").value;
+		document.getElementById("inputtedText").innerHTML = textValue;
+	} else {
+		inputtedTextDisplay.style.display = "none";
+	}
+
+	//Spelling Errors Toggle
+	var spellingErrorsToggle = document.getElementById("spellingErrorsToggle");
+	var spellingErrorsDisplay = document.getElementById("spellingErrorsDisplay");
+	if (spellingErrorsToggle.checked == true){
+		spellingErrorsDisplay.style.display = "block";
+	} else {
+		spellingErrorsDisplay.style.display = "none";
+	}
+
+	//Subjectivity Toggle
+	var subjectivityToggle = document.getElementById("subjectivityToggle");
+	var subjectivityDisplay = document.getElementById("subjectivityDisplay");
+	if (subjectivityToggle.checked == true){
+		subjectivityDisplay.style.display = "block";
+	} else {
+		subjectivityDisplay.style.display = "none";
+	}
+
+	//Subjectivity Toggle
+	var objectivityToggle = document.getElementById("objectivityToggle");
+	var objectivityDisplay = document.getElementById("objectivityDisplay");
+	if (objectivityToggle.checked == true){
+		objectivityDisplay.style.display = "block";
+	} else {
+		objectivityDisplay.style.display = "none";
+	}
+
+	//Polarity Toggle
+	var polarityToggle = document.getElementById("polarityToggle");
+	var polarityDisplay = document.getElementById("polarityDisplay");
+	if (polarityToggle.checked == true){
+		polarityDisplay.style.display = "block";
+	} else {
+		polarityDisplay.style.display = "none";
+	}
+
+	displayResultsBox();
 }
 
 function displayResultsBox(){
-   document.getElementById("resultsBox").style.display = "block";
+	document.getElementById("resultsBox").style.display = "block";
 }
 
-function swapMethods(buttonName){
-   if(buttonName == 'file'){
-      document.getElementById('manualInput').style.display = "none";
-      document.getElementById('docUpload').style.display = "block";
-   }
-   else{
-      document.getElementById('manualInput').style.display = "block";
-      document.getElementById('docUpload').style.display = "none";
-   }
+function swapMethods(evt, method) {
+	var i, tabcontent, tablinks;
+	tabcontent = document.getElementsByClassName("tabcontent");
+	for (i = 0; i < tabcontent.length; i++) {
+		tabcontent[i].style.display = "none";
+	}
+	tablinks = document.getElementsByClassName("tablinks");
+	for (i = 0; i < tablinks.length; i++) {
+		tablinks[i].className = tablinks[i].className.replace(" active", "");
+	}
+	document.getElementById(method).style.display = "block";
+	evt.currentTarget.className += " active";
 }
 
 function toggleAnalysisFilter(){
-   var x = document.getElementById("analysisFilter");
+	var x = document.getElementById("analysisFilter");
 
-   if(x.style.display == "none"){
-      x.style.display = "block"
-   }
-   else{
-      x.style.display = "none";
-   }
+	if(x.style.display == "none"){
+		x.style.display = "block"
+	}
+	else{
+		x.style.display = "none";
+	}
 }
 
 function displayPolarity(polarityValue){
-   document.getElementById("polarityVal").innerHTML = polarityValue;
+	document.getElementById("polarityVal").innerHTML = polarityValue;
 }

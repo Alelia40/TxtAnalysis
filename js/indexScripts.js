9fbbcd32ed7208a3417589cc0153d231b5e5831c
Alex Li <alelia40@gmail.com>
2019-11-23 20:10:26 -0500

    adds dials

--- js/indexScripts.js
+++ js/indexScripts.js
@@ -1,6 +1,13 @@
 function onSubmitPressed() {
+	displayDials();
+	displayResultsBox();
 
+	setSubjectivityValue(10);
+	setObjectivityValue(10);
+}
 
+function displayDials(){
+	
 	//Show Input Text Toggle
 	var inputtedTextToggle = document.getElementById("inputtedTextToggle");
 	var inputtedTextDisplay = document.getElementById("inputtedTextDisplay");
@@ -47,8 +54,6 @@ function onSubmitPressed() {
 	} else {
 		polarityDisplay.style.display = "none";
 	}
-
-	displayResultsBox();
 }
 
 function displayResultsBox(){
@@ -80,6 +85,80 @@ function toggleAnalysisFilter(){
 	}
 }
 
-function displayPolarity(polarityValue){
+function setPolarityValue(polarityValue){
 	document.getElementById("polarityVal").innerHTML = polarityValue;
+}
+
+function setSubjectivityValue(subjectivityValue){
+	var opts = {
+		angle: -0.20, // The span of the gauge arc
+		lineWidth: 0.44, // The line thickness
+		radiusScale: 1, // Relative radius
+		pointer: {
+		  length: 0.6, // // Relative to gauge radius
+		  strokeWidth: 0.035, // The thickness
+		  color: '#000000' // Fill color
+		},
+		limitMax: true,     // If false, max value increases automatically if value > maxValue
+		limitMin: true,     // If true, the min value of the gauge will be fixed
+		colorStart: '#bde7f4',   // Colors
+		colorStop: '#32b3dd',    // just experiment with them
+		strokeColor: '#E0E0E0',  // to see which ones work best for you
+		generateGradient: true,
+		highDpiSupport: true,     // High resolution support
+		renderTicks: {
+			divisions: 5,
+			divWidth: 1.1,
+			divLength: 0.7,
+			divColor: '#333333',
+			subDivisions: 3,
+			subLength: 0.5,
+			subWidth: 0.6,
+			subColor: '#666666'
+		  }  
+	  };
+	  var target = document.getElementById('subDial'); // your canvas element
+	  var gauge = new Gauge(target).setOptions(opts); // create sexy gauge!
+	  gauge.setTextField(document.getElementById('subVal'));
+	  gauge.maxValue = 99; // set max gauge value
+	  gauge.setMinValue(0);  // Prefer setter over gauge.minValue = 0
+	  gauge.animationSpeed = 32; // set animation speed (32 is default value)
+	  gauge.set(subjectivityValue); // set actual value
+}
+
+function setObjectivityValue(objectivityValue){
+	var opts = {
+		angle: -0.20, // The span of the gauge arc
+		lineWidth: 0.44, // The line thickness
+		radiusScale: 1, // Relative radius
+		pointer: {
+		  length: 0.6, // // Relative to gauge radius
+		  strokeWidth: 0.035, // The thickness
+		  color: '#000000' // Fill color
+		},
+		limitMax: true,     // If false, max value increases automatically if value > maxValue
+		limitMin: true,     // If true, the min value of the gauge will be fixed
+		colorStart: '#bde7f4',   // Colors
+		colorStop: '#32b3dd',    // just experiment with them
+		strokeColor: '#E0E0E0',  // to see which ones work best for you
+		generateGradient: true,
+		highDpiSupport: true,     // High resolution support
+		renderTicks: {
+			divisions: 5,
+			divWidth: 1.1,
+			divLength: 0.7,
+			divColor: '#333333',
+			subDivisions: 3,
+			subLength: 0.5,
+			subWidth: 0.6,
+			subColor: '#666666'
+		  }  
+	  };
+	  var target = document.getElementById('objDial'); // your canvas element
+	  var gauge2 = new Gauge(target).setOptions(opts); // create sexy gauge!
+	  gauge2.setTextField(document.getElementById('objVal'));
+	  gauge2.maxValue = 99; // set max gauge value
+	  gauge2.setMinValue(0);  // Prefer setter over gauge.minValue = 0
+	  gauge2.animationSpeed = 32; // set animation speed (32 is default value)
+	  gauge2.set(objectivityValue); // set actual value
 }

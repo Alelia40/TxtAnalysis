f03e012af37c1d0bcb8c32cf23e7936045c53291
Alelia40 <35640542+Alelia40@users.noreply.github.com>
2019-12-05 16:20:10 -0500

    overwrites inputtext.py

--- api/ImportText.py
+++ api/ImportText.py
@@ -1,31 +1,19 @@
-#!:usr/bin/python
+#!/usr/bin/python
 #enable debugging
 import sys
 import cgitb
-from  pathlib import *
-
+import cgi
 cgitb.enable()
 
+print("Content-type: text/plain\r\n\r\n")
 
-#****
-#testfile = Path('/usr/lib/cgi-bin/textfiles/mytest.txt')
-#text = u"Goodbye Cruel World\n"
-#testfile.open('w').write(text)
-#****
+fs = cgi.FieldStorage()
 
-print("Content-Type: text/plain\r\n\r\n")
+filename = fs.getvalue("filename")
+text = fs.getvalue("text")
 
-if(len(sys.argv) == 1):
-    #print("Content-Type: text/plain\r\n\r\n")
-    print
-    print("No text given")
-
-if(len(sys.argv) > 1):
-    lastimport = Path('/usr/lib/cgi-bin/textfiles/lastimport.txt')
-    text = sys.argv[1].decode('utf-8') + '\n'
-    lastimport.open('w').write(text)
-    #print("Content-Type: text/plain\r\n\r\n")
-    print
-    print("File has been saved")
-#if(len(sys.argv) > 1):
-    #add name to file
+f = open("/usr/lib/cgi-bin/textfiles/" + filename,"w+")
+f.write(text)
+f.close()
+print
+print("{ \"Success\":true  }")

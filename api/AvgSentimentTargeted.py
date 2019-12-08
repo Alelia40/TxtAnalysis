3b8560541812c332d870b6ce63f7c469c15a501e
Daniel Kessler <djk172@case.edu>
2019-12-08 00:13:24 -0500

    test2

--- api/AvgSentimentTargeted.py
+++ api/AvgSentimentTargeted.py
@@ -21,7 +21,7 @@ subj = []
 count = 0
 for s in blob.sentences:
     words = s.split()
-    if subject in words or words.lower():
+    if str(subject) in words or words.lower():
         pol.append(s.polarity)
         subj.append(s.subjectivity)
         count += 1
@@ -38,6 +38,6 @@ else:
 
 print("Content-Type: text/plain\r\n\r\n")
 print("{\n\"Subject\":" + subject + ",")
-print("{\n\"CountFound\":" + str(count) + ",")
-print("{\n\"Polarity\":" + str(avg_pol) + ",")
+print("\n\"CountFound\":" + str(count) + ",")
+print("\n\"Polarity\":" + str(avg_pol) + ",")
 print("\"Subjectivity\":" + str(avg_subj) + "\n}")
